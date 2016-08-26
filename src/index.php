06d2687752a319a011462bc0e579e355bc8802f7
rakesh635 <rakesh635@gmail.com>
2016-08-26 11:45:36 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>This is sample PHP Application, with PHPUnit for DEMO.</h1>";
+echo "<h1>This is sample PHP Application, with PHPUnit for DEMO</h1>";
 echo "<br/><b/>Thank you !!";
 ?>
