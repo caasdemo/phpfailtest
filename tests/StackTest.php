27fba91a4259ea8bf70d720624d139b50b0f42c2
rakesh635 <rakesh635@gmail.com>
2016-09-04 13:09:50 +0530

    Update StackTest.php

--- tests/StackTest.php
+++ tests/StackTest.php
@@ -16,7 +16,7 @@ class StackTest extends PHPUnit_Framework_TestCase
     public function testPush(array $stack)
     {
         array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals('foo123', $stack[count($stack)-1]);
         $this->assertNotEmpty($stack);
 
         return $stack;
@@ -27,7 +27,7 @@ class StackTest extends PHPUnit_Framework_TestCase
      */
     public function testPop(array $stack)
     {
-        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals('foo123', array_pop($stack));
         $this->assertEmpty($stack);
     }
 }
